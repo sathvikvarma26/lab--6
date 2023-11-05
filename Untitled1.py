501a834bace83b88f7979d9f4f0b60b0674074a9
sathvikvarma26 <143284935+sathvikvarma26@users.noreply.github.com>
2023-11-05 11:42:43 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -2,3 +2,25 @@ import numpy as np
 import pandas as pd
 df=pd.read_excel("embeddingsdatasheet-1.xlsx")
 df
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Load the dataset
+df = pd.read_excel("embeddingsdatasheet-1.xlsx")
+
+#Loading 2 features having numeric values
+feature_a = df['embed_0']
+feature_b = df['embed_1']
+
+# Create a scatter plot
+plt.figure(figsize=(8, 6))
+plt.scatter(feature_a, feature_b, color='blue', alpha=0.5)
+
+# Add labels and title
+plt.xlabel('embed_0')
+plt.ylabel('embed_1')
+plt.title('Scatter Plot of embed_0 vs embed_1')
+
+# Show plot
+plt.grid(True)
+plt.show()
