70d4659ba397a088279e4e6fae2fd834c2a32909
sathvikvarma26 <143284935+sathvikvarma26@users.noreply.github.com>
2023-11-05 11:45:14 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -93,3 +93,39 @@ predictions = logistic_model.predict(X_test)
 # Calculate accuracy by comparing predicted labels to actual labels in the test set
 accuracy = accuracy_score(y_test, predictions)
 print(f"Accuracy of Logistic Regression on the test set: {accuracy * 100:.2f}%")
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+
+# Load the dataset
+df = pd.read_excel("embeddingsdatasheet-1.xlsx")
+
+# Assuming your target variable is 'target_variable'
+target_variable = df['Label']
+
+# Extracting features
+X = df[['embed_0', 'embed_1']]
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, target_variable, test_size=0.2, random_state=42)
+
+# Decision Tree Regressor
+reg_tree = DecisionTreeRegressor(random_state=42)
+reg_tree.fit(X_train, y_train)
+y_pred_tree = reg_tree.predict(X_test)
+mse_tree = mean_squared_error(y_test, y_pred_tree)
+print(f"Decision Tree Mean Squared Error: {mse_tree}")
+
+# k-NN Regressor
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+knn_regressor = KNeighborsRegressor(n_neighbors=5)
+knn_regressor.fit(X_train_scaled, y_train)
+y_pred_knn = knn_regressor.predict(X_test_scaled)
+mse_knn = mean_squared_error(y_test, y_pred_knn)
+print(f"k-NN Regressor Mean Squared Error: {mse_knn}")
